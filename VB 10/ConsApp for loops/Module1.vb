2330b1dfa6af18551e9817f0f704daeefb7ea371
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:39:49 -0400

    First commit

---
+++ VB 10/ConsApp for loops/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim i As Integer
+
+        For i = 1 To 10
+            Console.WriteLine(i)
+        Next
+
+        Console.WriteLine("")
+
+        Dim s As String = "hello"
+        Dim c As Char
+
+        For Each c In s
+            Console.WriteLine(c)
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: Visual Basic 10 - For Loops
+Published: 7/11/2010 12:05:57 PM
+Link: https://www.youtube.com/watch?v=ljMFAHjCJ20
+
+Description: Learn about FOR and FOR EACH
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
